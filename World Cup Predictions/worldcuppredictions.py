c807660433e29b2eb2b7faa2ed14aa297b435e90
Andre <Andre@hedgology.com>
2018-07-25 03:47:40 -0400

    New Additions

--- World Cup Predictions/worldcuppredictions.py
+++ World Cup Predictions/worldcuppredictions.py
@@ -45,3 +45,101 @@ plt.xlabel('Number of player', fontsize=25)
 plt.ylabel('Player age', fontsize=25)
 sns.countplot(x='Age', data=FIFA18, palette='hls')
 plt.show()
+
+FIFA18['Position'] = FIFA18['Preferred Positions'].str.split().str[0]
+
+plt.figure(figsize=(16,8))
+sns.set_style('whitegrid')
+plt.title('Grouping player by Age', fontsize=30, fontweight='bold', y=1.05)
+plt.xlabel('Number of players', fontsize=25)
+plt.ylabel('Players Age', fontsize=25)
+sns.countplot(x='Age', data=FIFA18, palette='hls')
+plt.show()
+
+plt.figure(figsize=(16,8))
+sns.set_style('whitegrid')
+plt.title('Grouping players by Overall', fontsize=30, fontweight='bold', y=1.05)
+plt.xlabel('Number of players', fontsize=25)
+plt.ylabel('Players Age', fontsize=25)
+sns.countplot(x='Overall', data=FIFA18, palette='hls')
+plt.show()
+
+plt.figure(figsize=(16,8))
+sns.set_style('whitegrid')
+plt.title('Grouping players by Preferred Position', fontsize=30, fontweight='bold', y=1.05)
+plt.xlabel('Number of players', fontsize=25)
+plt.ylabel('Players Age', fontsize=25)
+sns.countplot(x='Position', data=FIFA18, palette='hls')
+plt.show()
+
+FIFA18['Nationality'].value_counts().head(25)
+
+sorted_players = FIFA18.sort_values(['ValueNum'], ascending= False).head(20)
+players = sorted_players[['Name', 'Age', 'Nationality', 'Club', 'Position', 'Value']].values
+
+from IPython.display import HTML, display
+
+table_content = ''
+for row in players:
+    HTML_row = '<tr>'
+    HTML_row += '<td>' + str(row[0]) + '</td>'
+    HTML_row += '<td>' + str(row[1]) + '</td>'
+    HTML_row += '<td>' + str(row[2]) + '</td>'
+    HTML_row += '<td>' + str(row[3]) + '</td>'
+    HTML_row += '<td>' + str(row[4]) + '</td>'
+    HTML_row += '<td>' + str(row[5]) + '</td>'
+
+    table_content += HTML_row + '</tr>'
+
+display(HTML(
+    '<table><tr><th>Name</th><th>Age</th><th>Nationality</th><th>Club</th><th>Position</th><th>Value</th></tr>{}</table>'.format(table_content))
+)
+
+plt.figure(figsize=(16,8))
+sns.set_style('whitegrid')
+plt.title('Players Value according to their Age and Overall', fontsize=30, fontweight='bold', y=1.05)
+plt.xlabel('Age', fontsize=25)
+plt.ylabel('Overall', fontsize=25)
+
+age = FIFA18['Age'].values
+overall = FIFA18['Overall'].values
+value = FIFA18['ValueNum'].values
+
+plt.scatter(age, overall, s = value/10000, edgecolors='black')
+plt.show()
+
+sorted_players = FIFA18.sort_values(['WageNum'], ascending=False).head(20)
+players = sorted_players[['Name', 'Age', 'Nationality', 'Club', 'Position', 'Wage']].values
+
+from IPython.display import HTML, display
+
+table_content = ''
+for row in players:
+    HTML_row = '<tr>'
+    HTML_row += '<td>' + str(row[0]) + '</td>'
+    HTML_row += '<td>' + str(row[1]) + '</td>'
+    HTML_row += '<td>' + str(row[2]) + '</td>'
+    HTML_row += '<td>' + str(row[3]) + '</td>'
+    HTML_row += '<td>' + str(row[4]) + '</td>'
+    HTML_row += '<td>' + str(row[5]) + '</td>'
+
+    table_content += HTML_row + '</tr>'
+
+display(HTML(
+    '<table><tr><th>Name</th><th>Age</th><th>Nationality</th><th>Club</th><th>Position</th><th>Wage</th></tr>{}</table>'.format(table_content))
+)
+
+plt.figure(figsize=(16,8))
+sns.set_style('whitegrid')
+plt.title('Player Wage according to their Age and Overall', fontsize=30, fontweight='bold', y=1.05)
+plt.xlabel('Age', fontsize=25)
+plt.ylabel('Overall', fontsize= 25)
+age = FIFA18['Age'].values
+overall = FIFA18['Overall'].values
+value = FIFA18['WageNum'].values
+
+plt.scatter(age, overall, s = value/500, edgecolors='black', color='red')
+plt.show()
+
+FIFA18 = FIFA18[['Name', 'Age', 'Nationality', 'Overall', 'Potential', 'Club', 'Position', 'Value', 'Wage']]
+FIFA18.head(10)
