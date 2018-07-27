2923a853b86c82c6efaf8b684e46d2d5d113f947
Andre <Andre@hedgology.com>
2018-07-27 02:47:00 -0400

    Supervised machine learning practice

--- Machine Learning/iris_supervisedml.py
+++ Machine Learning/iris_supervisedml.py
@@ -58,3 +58,8 @@ diabetes_y_test = diabetes.target[-20]
 regr = linear_model.LinearRegression()
 
 regr.fit(diabetes_X_train, diabetes_y_train)
+
+print('Input Values')
+print(diabetes_X_test)
+
+diabetes_y_pred = regr.predict(diabetes_X_test)
