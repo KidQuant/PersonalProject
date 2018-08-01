d74453fa0435f332d03ea29a5dbd5f455886de87
Andre <Andre@hedgology.com>
2018-08-01 04:17:46 -0400

    Changes to folder

---
+++ Machine_Learning/Deceptive_Hotel_Reviews/HotelReviewsml.py
@@ -0,0 +1,25 @@
+import os
+import fnmatch
+from textblob import TextBlob
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+import regex as re
+import operator
+from sklearn.svm import SVC, LinearSVC
+from sklearn import metrics
+from sklearn import svm
+from sklearn.grid_search import GridSearchCV
+import pickle
+from nltk.corpus import stopwords
+
+
+path = 'op_spam_train/'
+
+label = []
+
+configfiles = [os.path.join(subdir,f)
+for subdir, dirs, files in os.walk(path)
+    for f in fnmath.filter(files, '*.txt')]
+
+len(configfiles)
