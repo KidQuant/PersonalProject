88dfa7c66f6fafed5f2d647d3535563454ce3098
Andre <Andre@hedgology.com>
2018-08-01 23:59:24 -0400

    Creating a re-sampled dataframe.

--- Machine_Learning/Hotel_Predictions/Hotel_Predictions.py
+++ Machine_Learning/Hotel_Predictions/Hotel_Predictions.py
@@ -24,3 +24,35 @@ count_classes.plot(kind = 'bar')
 plt.title('Booking or Not booking')
 plt.xlabel('Class')
 plt.ylabel('Frequency')
+
+df['date_time'] = pd.to_datetime(df['date_time'])
+df['year'] = df['date_time'].dt.year
+df['month'] = df['date_time'].dt.month
+
+df['srch_ci'] = pd.to_datetime(df['srch_ci'], infer_datetime_format = True, errors = 'coerce')
+df['srch_co'] = pd.to_datetime(df['srch_co'], infer_datetime_format = True, errors = 'coerce')
+
+df['plan_time'] = ((df['srch_ci'] - df['date_time'])/np.timedelta64(1,'D')).astype(float)
+df['hotel_nights'] = ((df['srch_co'] - df['srch_ci'])/np.timedelta64(1,'D')).astype(float)
+
+cols_to_drop = ['date_time', 'srch_ci', 'srch_co', 'user_id']
+df.drop(cols_to_drop, axis=1, inplace=True)
+
+correlation = df.corr()
+plt.figure(figsize=(18,18))
+sns.heatmap(correlation, vmax=1, square=True, annot=True,cmap='viridis')
+plt.title('Correlation between different features')
+
+booking_indices = df[df.is_booking == 1].index
+random_indices = np.random.choice(booking_indices, len(df.loc[df.is_booking == 1]), replace = False)
+booking_sample = df.loc[random_indices]
+
+not_booking = df[df.is_booking == 0].index
+random_indices = np.random.choice(not_booking, sum(df['is_booking']), replace= False)
+not_booking_sample = df.loc[random_indices]
+
+df_new = pd.concat([not_booking_sample, booking_sample], axis = 0)
+
+print('Percentage of not booking clicks: ', len(df_new[df_new.is_booking == 0]) / len(df_new))
+print('Percentage of booking clicks: ', len(df_new[df_new.is_booking == 1]) / len(df_new))
+print('Total number of records in resampled data: ', len(df_new))
