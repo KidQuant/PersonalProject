043e9230d397e0b0c25c10bc90934ba3a0daf357
Andre <Andre@hedgology.com>
2018-08-05 13:37:21 -0400

    Code used to extract rejection letters from personal email account.

---
+++ DataScience/Banking_Database/rejections.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import email, getpass, imaplib, os, re
+from datetime import datetime
+
+detach_dir = '.'
+user = input('Enter your GMail username:')
+pwd = getpass.getpass('Enter your password:')
+
+m = imaplib.IMAP4_SSL('imap.gmail.com')
+m.login(user, pwd)
+
+m.select('"Job Rejection"')
+
+resp, items = m.search(None, "All")
+items = items[0].split()
+
+text = []
+dates = []
+subjects = []
+
+for emailid in items:
+
+    # Fetch everything from the id
+    resp, data = m.fetch(emailid, "(RFC822)")
+
+    # Get the content
+    email_body = data[0][1]
+
+    # Convert to mail object
+    mail = email.message_from_bytes(email_body)
+
+    # Get subject
+    subjects.append(email.header.decode_header(mail['Subject'])[0][0])
+
+    # Get date
+    date_tuple = email.utils.parsedate_tz(mail['Date'])
+    dates.append(datetime.fromtimestamp(email.utils.mktime_tz(date_tuple)))
+
+    # Get text
+    if mail.is_multipart():
+        text.append(mail.get_payload(0).get_payload())
+    else:
+        text.append(mail.get_payload())
+
+df = pd.DataFrame(data={'Date':dates, 'Subject':subjects, 'Text':text})
+df.head()
+
+df['Time'] = df['Date'].apply(lambda x: x.time())
+df['Day'] = df['Date'].apply(lambda x: x.weekday()).map({0:'Mon', 1:'Tues', 2:'Weds', 3:'Thur', 4:'Fri', 5:'Sat', 6:'Sun'})
+df['Hour'] = df['Time'].apply(lambda x: x.hour)
+df = df[['Date', 'Time', 'Day', 'Hour', 'Subject', 'Text']]
+
+df.head()
+
+df.info()
+
+df.to_csv('andres_rejections.csv', index=False)
