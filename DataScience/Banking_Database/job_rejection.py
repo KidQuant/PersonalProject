0b600c68bfe0205fb2039e23c3bc62b926107f62
Andre <Andre@hedgology.com>
2018-08-05 15:59:04 -0400

    New script for job rejection data analysis

---
+++ DataScience/Banking_Database/job_rejection.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+from nltk.corpus import stopwords
+from collections import Counter
+import nltk
+%matplotlib inline
+sns.set(style = 'whitegrid', palette = 'muted', font_scale=1.3)
+
+df = pd.read_csv('andres_rejections.csv')
