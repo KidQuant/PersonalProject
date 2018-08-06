2b98617d2d39d4eac83e5711658e5f8e40b5a0ee
Andre <Andre@hedgology.com>
2018-08-06 01:44:22 -0400

    New commits.

--- DataScience/Banking_Database/Banking_Database.py
+++ DataScience/Banking_Database/Banking_Database.py
@@ -3,7 +3,7 @@ import numpy as np
 
 df = pd.read_csv('company_database.csv', encoding = 'latin1', index_col = False)
 
-df.loc[9:9]
+df.loc[0:15]
 
 networking = pd.read_csv('networking.csv')
 
@@ -12,13 +12,13 @@ networking.tail()
 
 del df5
 
-df5 = pd.DataFrame({'Name':['Marissa Frey', 'Cheyenne Dougherty', 'Camille Pasquier'],
-                    'Firm':['Citigroup', 'Citigroup', 'Citigroup'],
-                    'Location':['New York, New York', 'New York, New York', 'New York, New York'],
-                    'Email':['jacqueline.waugh@citi.com', 'cheyenne.dougherty@citi.com', 'camille.pasquier@citi.com'],
-                    'Have Contacted?':['No', 'No', 'No'],
-                    'Recieved Response?':['N/A', 'N/A', 'N/A'],
-                    'Followed Up?':['N/A', 'N/A', 'N/A']})
+df5 = pd.DataFrame({'Name':['Laura Dlug'],
+                    'Firm':['Deutsche Bank'],
+                    'Location':['New York, New York'],
+                    'Email':['Laura.dlug@db.com'],
+                    'Have Contacted?':['No'],
+                    'Recieved Response?':['N/A'],
+                    'Followed Up?':['N/A']})
 
 networking  = networking.append(df5, ignore_index=True, sort=False)
 
@@ -32,4 +32,4 @@ networking.drop_duplicates()
 
 networking.to_csv('networking.csv', index = False)
 
-networking
+networking.loc[]
