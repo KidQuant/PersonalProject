a44ab6b1a26b7142b3b0f5edcaa7e62222aa9aa6
Andre <Andre@hedgology.com>
2018-08-05 23:54:54 -0400

    New changes involving sorting values.

--- DataScience/Banking_Database/Banking_Database.py
+++ DataScience/Banking_Database/Banking_Database.py
@@ -3,30 +3,33 @@ import numpy as np
 
 df = pd.read_csv('company_database.csv', encoding = 'latin1', index_col = False)
 
-df.loc[29:29]
+df.loc[9:9]
 
 networking = pd.read_csv('networking.csv')
-
-networking.tail()
-
-del df5
-
-df5 = pd.DataFrame({'Name':['Kathleen Goettelmann'],
-                    'Firm':['Lazard'],
-                    'Email':['kathleen.goettelmann@lazard.com'],
-                    'Have Contacted?':['No'],
-                    'Recieved Response?':['N/A'],
-                    'Followed Up?':['N/A']})
-
-networking  = networking.append(df5, ignore_index=True, sort=False)
 
 networking.head()
 networking.tail()
 
-networking.sort_values(by = ['Firm'], inplace=True)
+del df5
+
+df5 = pd.DataFrame({'Name':['Jessica Chisholm', 'Sean Hille'],
+                    'Firm':['Bank of America', 'Bank of America'],
+                    'Location':['New York, New York', 'New York, New York'],
+                    'Email':['jessica.chisholm@bankofamerica.com', 'sean.hille@bankofamerica.com'],
+                    'Have Contacted?':['No', 'No'],
+                    'Recieved Response?':['N/A', 'N/A'],
+                    'Followed Up?':['N/A', 'N/A']})
+
+networking  = networking.append(df5, ignore_index=True, sort=False)
+
+networking.tail()
+
+networking.sort_values(by = ['Firm', 'Name'], inplace=True)
 
 networking.duplicated().sum()
 
 networking.drop_duplicates()
 
 networking.to_csv('networking.csv', index = False)
+
+networking
