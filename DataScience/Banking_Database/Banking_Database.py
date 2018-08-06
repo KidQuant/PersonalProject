1f1039508612ed6dc64de23ca2b5eb46de1d5c06
Andre <Andre@hedgology.com>
2018-08-05 21:48:23 -0400

    Changes to the networking file and csv.

--- DataScience/Banking_Database/Banking_Database.py
+++ DataScience/Banking_Database/Banking_Database.py
@@ -5,16 +5,16 @@ df = pd.read_csv('company_database.csv', encoding = 'latin1', index_col = False)
 
 df.loc[1:3]
 
-networking = pd.read_csv('networking.csv', index_col = 0)
+networking = pd.read_csv('networking.csv')
 
 networking.head()
 networking.tail()
 
 del df5
 
-df5 = pd.DataFrame({'Name':['Courtney Goldsmith'],
-                    'Firm':['Barclays'],
-                    'Email':['courtney.goldsmith@barcap.com'],
+df5 = pd.DataFrame({'Name':['Nick Bradford'],
+                    'Firm':['RBC Capital Markets'],
+                    'Email':['nick.bradford@rbccm.com'],
                     'Have Contacted?':['No'],
                     'Recieved Response?':['N/A'],
                     'Followed Up?':['N/A']})
@@ -24,4 +24,10 @@ networking  = networking.append(df5, ignore_index=True, sort=False)
 networking.head()
 networking.tail()
 
-networking.to_csv('networking.csv')
+networking.sort_values(by = ['Firm'])
+
+networking.duplicated()
+
+networking.drop_duplicates()
+
+networking.to_csv('networking.csv', index = False)
