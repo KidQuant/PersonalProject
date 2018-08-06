afa43bb4674d220e402346922712efeccfd6e548
Andre <Andre@hedgology.com>
2018-08-06 00:20:59 -0400

    Made some additions to the dataframe and csv file.

--- DataScience/Banking_Database/Banking_Database.py
+++ DataScience/Banking_Database/Banking_Database.py
@@ -12,13 +12,13 @@ networking.tail()
 
 del df5
 
-df5 = pd.DataFrame({'Name':['Jessica Chisholm', 'Sean Hille'],
-                    'Firm':['Bank of America', 'Bank of America'],
-                    'Location':['New York, New York', 'New York, New York'],
-                    'Email':['jessica.chisholm@bankofamerica.com', 'sean.hille@bankofamerica.com'],
-                    'Have Contacted?':['No', 'No'],
-                    'Recieved Response?':['N/A', 'N/A'],
-                    'Followed Up?':['N/A', 'N/A']})
+df5 = pd.DataFrame({'Name':['Marissa Frey', 'Cheyenne Dougherty', 'Camille Pasquier'],
+                    'Firm':['Citigroup', 'Citigroup', 'Citigroup'],
+                    'Location':['New York, New York', 'New York, New York', 'New York, New York'],
+                    'Email':['jacqueline.waugh@citi.com', 'cheyenne.dougherty@citi.com', 'camille.pasquier@citi.com'],
+                    'Have Contacted?':['No', 'No', 'No'],
+                    'Recieved Response?':['N/A', 'N/A', 'N/A'],
+                    'Followed Up?':['N/A', 'N/A', 'N/A']})
 
 networking  = networking.append(df5, ignore_index=True, sort=False)
 
