2b3edc0681524e564e35438e620ca0ea038195d8
Andre <Andre@hedgology.com>
2018-08-06 04:23:05 -0400

    New code for missing table headers.

---
+++ web_scraping.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = 'http://www.hubertiming.com/results/2017GPTR10K'
+html = urlopen(url)
+
+soup = BeautifulSoup(html, 'lxml')
+type(soup)
+
+title = soup.title
+print(title)
+
+text = soup.get_text()
+
+soup.find_all('a')
+
+all_links = soup.find_all('a')
+for link in all_links:
+    print(link.get('href'))
+
+rows = soup.find_all('tr')
+print(rows[:10])
+
+for row in rows:
+    row_td = row.find_all('td')
+    print(row_td)
+    type(row_td)
+
+str_cells = str(row_td)
+cleantext = BeautifulSoup(str_cells, 'lxml').get_text()
+print(cleantext)
+
+import re
+
+list_rows = []
+for row in rows:
+    cells = row.find_all('td')
+    str_cells = str(cells)
+    clean = re.compile('<.*?>')
+    clean2 = (re.sub(clean, '',str_cells))
+    list_rows.append(clean2)
+    print(clean2)
+    type(clean2)
+
+df = pd.DataFrame(list_rows)
+df.head(10)
+
+df1 = df[0].str.split(',', expand=True)
+df1.head()
+
+df[0] = df1[0].str.strip('[')
+df1.head(10)
+
+col_labels = soup.find_all('th')
