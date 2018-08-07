f6a1c3aa21ab3f1a59afecb6c44ec1ac7803d1ed
Andre <Andre@hedgology.com>
2018-08-06 23:50:46 -0400

    New code for creating a list of review and merging the list to a df.

--- Machine_Learning/Deceptive_Hotel_Reviews/HotelReviewsml.py
+++ Machine_Learning/Deceptive_Hotel_Reviews/HotelReviewsml.py
@@ -23,4 +23,30 @@ for subdir, dirs, files in os.walk(path)
 
 len(configfiles)
 
-config
+configfiles[1]
+
+for f in configfiles:
+    c = re.search('(trut|deceptiv)\w',f)
+    label.append(c.group())
+
+labels = pd.DataFrame(label, columns = ['Labels'])
+
+labels.head(5)
+
+review = []
+directory = os.path.join('op_spam_v1.4')
+for subdir, dirs, files in os.walk(directory):
+    print(subdir)
+    for file in files:
+        if fnmatch.filter(files, '*.txt'):
+            f = open(os.path.join(subdir, file), 'r')
+            a = f.read()
+            review.append(a)
+
+reviews = pd.DataFrame(review, columns = ['HotelReviews'])
+
+reviews.head(5)
+
+results = pd.merge(reviews, labels, right_index = True, left_index = True)
+
+results['HotelReviews'] = results['HotelReviews'].map(lambda x: x.lower())
