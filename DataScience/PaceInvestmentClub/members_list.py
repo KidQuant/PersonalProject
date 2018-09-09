aaca459f9a896a6d3cb99e97d8694855b372796d
Sealy <as70906n@pace.edu>
2018-09-09 15:20:18 -0400

    New code for formatting date; fixed code for filtering.

--- DataScience/PaceInvestmentClub/members_list.py
+++ DataScience/PaceInvestmentClub/members_list.py
@@ -5,26 +5,28 @@ import numpy as np
 
 members = pd.read_csv('members_list.csv', encoding = 'latin1', index_col = False)
 
-members
-
 members = members.drop(['Middle Initial', 'Title', 'Phone','Mailing Address','Birthday'], axis =1)
-
-members
 
 members['Last Name'] = members['Last Name'].str.capitalize()
 
 members['First Name'] = members['First Name'].str.capitalize()
 
-members
+members = members.rename(index=str, columns = {'Last Name':'Last','First Name':'First','Last Login':'Last_Login' ,'Join Date': 'Joined', 'Email Address': 'Email'})
 
-new_members_list = members.drop(members[members['Join Date'] < '2016-01-01'].index)
+members['Joined'] = pd.to_datetime(members['Joined'])
+
+members['Last_Login'] = pd.to_datetime(members['Last_Login'])
+
+new_members_list = members[(members['Joined'] > '2016-01-01')]
 
 new_members_list.reset_index(inplace=True)
 
 new_members_list
 
-new_members_list.drop('index', axis =1, inplace=True)
+new_members_list =  new_members_list.drop('index', axis =1)
 
 new_members_list
+
+new_members_list[(new_members_list['Joined'] > '2018-09-01')]
 
 new_members_list.to_csv('new_members_list.csv')
