83b6afd989bf1c35b25ce3c4be997768b224e25f
Andre Sealy <andresealy@Andres-MacBook.local>
2018-09-14 11:07:29 -0400

    New consumer credit model...

---
+++ Consumer_Complaints/Consumer_Complaints.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+df = pd.read_csv('Consumer_Complaints/Consumer_Complaints.csv')
+
+df.head()
+
+col = ['Product', 'Consumer complaint narrative']
+df = df[col]
+df = df[pd.notnull(df['Consumer complaint narrative'])]
+
+df.columns = ['Product', 'Consumer_complaint_narrative']
+
+df['category_id'] = df['Product'].factorize()[0]
+
+category_id_df = df[['Product','category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id','Product']].values)
+df.head()
+
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(8,6))
+df.groupby('Product').Consumer_complaint_narrative.count().plot.bar(ylim=0)
+plt.show()
+
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+
+features = tfidf.fit_transform(df.Consumer_complaint_narrative).toarray()
+labels = df.category_id
+features.shape
