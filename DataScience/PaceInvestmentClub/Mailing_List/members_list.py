8a1d18e04e6f553a729376f7efbae0f97e4d128c
Sealy <as70906n@pace.edu>
2018-09-14 09:52:51 -0400

    New members and new member files...

--- DataScience/PaceInvestmentClub/Mailing_List/members_list.py
+++ DataScience/PaceInvestmentClub/Mailing_List/members_list.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 
+
 #Cleaning data for PIC members list
 
 members = pd.read_csv('DataScience/PaceInvestmentClub/Mailing_List/members_list.csv', encoding = 'latin1')
@@ -28,35 +29,26 @@ for index, row in new_members_list.iterrows():
     if row['Last'] == 'Caoimh':
         new_members_list.drop(index, inplace=True)
 
-<<<<<<< HEAD
-new_members_list.head()
-
-new_members_list.to_csv('new_members_list.csv', index = False)
-=======
 new_members_list
 
-new_members_list[new_members_list['Joined'] == '2018-09-11']
+new_members_list[new_members_list['Joined'] == '2018-09-14']
 
 new_members_list.to_csv('DataScience/PaceInvestmentClub/Mailing_List/new_members_list.csv', index = False)
->>>>>>> 53493880916a30b479a672abfcb6aa318e47e1d1
 
 new_members_list = pd.read_csv('new_members_list.csv')
 
 new_members_list[(new_members_list['Joined'] > '2018-09-01')]
 
-<<<<<<< HEAD
-new_members_list[new_members_list['First'] == '']
-=======
 new_members_list.head()
 
 new_members_list = pd.read_csv('DataScience/PaceInvestmentClub/Mailing_List/new_members_list.csv')
 
-df = pd.DataFrame({'Last':['Kujawski'],
-                   'First':['Matthew'],
+df = pd.DataFrame({'Last':['Schilling'],
+                   'First':['Brittany'],
                    'Groups':['New Accounts'],
                    'Joined':['2018-09-13'],
                    'Last_Login':['2018-09-13'],
-                   'Email':['mk29856n@pace.edu']})
+                   'Email':['bs90466n@pace.edu']})
 
 new_members_list = new_members_list.append(df, ignore_index = True, sort = False)
 
@@ -65,4 +57,3 @@ new_members_list.tail()
 new_members_list.to_csv('DataScience/PaceInvestmentClub/Mailing_List/new_members_list.csv', index = False)
 
 new_members_list.tail()
->>>>>>> 53493880916a30b479a672abfcb6aa318e47e1d1
