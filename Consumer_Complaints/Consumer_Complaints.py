dd01750dd809ca144afc493d1589135de36a75ef
Sealy <as70906n@pace.edu>
2018-09-18 21:38:29 -0400

    New code changes

--- Consumer_Complaints/Consumer_Complaints.py
+++ Consumer_Complaints/Consumer_Complaints.py
@@ -1,7 +1,5 @@
 import pandas as pd
-
-df = pd.read_csv('Consumer_Complaints/Consumer_Complaints.csv')
-
+df = pd.read_csv('Machine_Learning/Consumer_Complaints/Consumer_Complaints.csv')
 df.head()
 
 col = ['Product', 'Consumer complaint narrative']
@@ -11,7 +9,6 @@ df = df[pd.notnull(df['Consumer complaint narrative'])]
 df.columns = ['Product', 'Consumer_complaint_narrative']
 
 df['category_id'] = df['Product'].factorize()[0]
-
 category_id_df = df[['Product','category_id']].drop_duplicates().sort_values('category_id')
 category_to_id = dict(category_id_df.values)
 id_to_category = dict(category_id_df[['category_id','Product']].values)
@@ -23,16 +20,16 @@ df.groupby('Product').Consumer_complaint_narrative.count().plot.bar(ylim=0)
 plt.show()
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+tfidf = TfidfVectorizer(sublinear_tf = True, min_df = 5, norm = '12', encoding='latin-1', ngram_range=(1,2), stop_words='english')
 
 features = tfidf.fit_transform(df.Consumer_complaint_narrative).toarray()
 labels = df.category_id
-features.shape
 
 from sklearn.feature_selection import chi2
+import numpy as np
 
+N = 2
 for Product, category_id in sorted(category_to_id.items()):
-    features_chi2 = chi2(features, labels == categories_id)
-
-asd
+    features_chi2 = chi2(features, labels == category_id)
+    indices = np.argsort(features_chi2[0])
+    features_names = np.array(tfidf.get_feature_name())
