3b9d8ece26fc4db2eb444c141d8239d29b8f93a6
Sealy <as70906n@pace.edu>
2018-09-22 19:50:10 -0400

    Fixed drop axis problem.

--- statistics_probability/fifa_match_simulation/fifa_match_simulation.py
+++ statistics_probability/fifa_match_simulation/fifa_match_simulation.py
@@ -11,14 +11,61 @@ world_cup = pd.read_csv('statistics_probability/fifa_match_simulation/World Cup 
 results = pd.read_csv('statistics_probability/fifa_match_simulation/results.csv')
 
 world_cup.head()
-
 results.head()
 
 winner = []
 for i in range (len(results['home_team'])):
     if results['home_score'][i] > results['away_score'][i]:
         winner.append(results['home_team'][i])
-    elif results['home_score'][i < results ['away_score'][i]:
+    elif results['home_score'][i] < results['away_score'][i]:
         winner.append(results['away_team'][i])
+    else:
+        winner.append('Draw')
 
-        
+results['winning_team'] = winner
+
+results['goal_difference'] = np.absolute(results['home_score'] - results['away_score'])
+
+results.head()
+
+df = results[(results['home_team'] == 'Nigeria') | (results['away_team'] == 'Nigeria')]
+nigeria = df.iloc[:]
+nigeria.head()
+
+year = []
+for row in nigeria['date']:
+    year.append(int(row[:4]))
+nigeria['match_year'] = year
+nigeria_1930 = nigeria[nigeria.match_year >= 1930]
+nigeria_1930.count()
+
+worldcup_teams = ['Australia', 'Iran', 'Japan', 'Korean Republic',
+                  'Saudi Arabia', 'Egypt', 'Morocco', 'Nigeria',
+                  'Senegal', 'Tunisia', 'Costa Rica', 'Mexico',
+                  'Panama', 'Argentina', 'Brazil', 'Colombia',
+                  'Peru', 'Uruguay', 'Belgium', 'Croatia',
+                  'Denmark', 'England', 'France', 'Germany',
+                  'Iceland', 'Poland', 'Portugal', 'Russia',
+                  'Serbia', 'Spain', 'Sweden', 'Switzerland']
+
+df_teams_home = results[results['home_team'].isin(worldcup_teams)]
+df_teams_away = results[results['away_team'].isin(worldcup_teams)]
+df_teams = pd.concat((df_teams_home, df_teams_away))
+df_teams.drop_duplicates()
+df_teams.count()
+
+year = []
+for row in df_teams['date']:
+    year.append(int(row[:4]))
+df_teams['match_year'] = year
+df_teams_1930 = df_teams[df_teams.match_year >= 1930]
+df_teams_1930.head()
+
+df_teams_1930 = df_teams.drop(['date', 'home_score', 'away_score', 'tournament', 'city', 'country', 'goal_difference', 'match_year'], axis =1)
+
+df_teams_1930 = df_teams_1930.reset_index(drop=True)
+df_teams_1930.loc[df_teams_1930.winning_team == df_teams_1930.home_team, 'winning_team'] = 2
+df_teams_1930.loc[df_teams_1930.winning_team == 'Draw', 'winning_team'] = 1
+df_teams_1930.loc[df_teams_1930.winning_team == df_teams_1930.away_team, 'winning_team'] = 0
+
+df_teams_1930.head()
