4599c2c073a91e4b84c1da23c7201562b42f497b
Andre Sealy <andresealy@Andres-MacBook.local>
2018-09-25 12:56:58 -0400

    New code for rearranging columns in Pandas

--- DataScience/Banking_Database/Banking_Database.py
+++ DataScience/Banking_Database/Banking_Database.py
@@ -5,7 +5,7 @@ df = pd.read_csv('DataScience/Banking_Database/company_database.csv', encoding =
 
 df.loc[0:60]
 
-df[df['Company Name'] == 'SunTrust']
+df[df['Company Name'] == 'Duff & Phelps']
 
 networking = pd.read_csv('DataScience/Banking_Database/networking.csv')
 
@@ -23,7 +23,7 @@ df5 = pd.DataFrame({'Name':['Michael Hasselt'],
                     'Recieved Response?':['N/A'],
                     'Followed Up?':['N/A']})
 
-networking  = networking.append(df5, ignore_index=True, sort=False)
+networking  = networking.append(df5, ignore_index= True, sort = False)
 
 networking.sort_values(by = ['Firm', 'Name'], inplace=True)
 
@@ -35,11 +35,11 @@ networking.to_csv('networking.csv', index = False)
 
 networking
 
-networking[networking['Firm'] == 'Lazard']
+networking[networking['Firm'] == 'PNC']
 
 networking['Location'][3:4] = 'Chicago, Illinois'
 networking['Email'][57:58] = 'rebecca.mcadams@cowen.com'
-networking['Have Contacted?'][2:3] = 'No'
-networking['Recieved Response?'][3:4] = 'No'
-networking['Followed Up?'][3:4] = 'No'
+networking['Have Contacted?'][77:78] = 'Yes'
+networking['Recieved Response?'][77:78] = 'No'
+networking['Followed Up?'][77:78] = 'No'
 networking[(networking['Have Contacted?'] == 'Yes') & (networking['Firm'] == 'Lazard')]
