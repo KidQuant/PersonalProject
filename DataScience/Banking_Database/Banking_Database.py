8db41ec486f5cf331f6419579cdb7b15ec44f641
Sealy <as70906n@pace.edu>
2018-09-26 09:50:16 -0400

    New networking in banking database

--- DataScience/Banking_Database/Banking_Database.py
+++ DataScience/Banking_Database/Banking_Database.py
@@ -5,7 +5,7 @@ df = pd.read_csv('DataScience/Banking_Database/company_database.csv', encoding =
 
 df.loc[0:60]
 
-df[df['Company Name'] == 'Duff & Phelps']
+df[df['Company Name'] == 'Raymond James Financial Inc.']
 
 networking = pd.read_csv('DataScience/Banking_Database/networking.csv')
 
@@ -14,23 +14,23 @@ networking.tail()
 
 del df5
 
-df = pd.DataFrame({'Name':['Melinda Pesapane'],
-                   'Firm':['Duff & Phelps'],
-                   'Location':['New York, New York'],
-                   'Email':['melinda.pesapane@duffandphelps.com'],
+df = pd.DataFrame({'Name':['Grace Blankenship'],
+                   'Firm':['Raymond James'],
+                   'Location':['Tampa, Flordia'],
+                   'Email':['grace.blankenship@raymondjames.com'],
                    'Have Contacted?':['No'],
                    'Recieved Response?':['N/A'],
                    'Followed Up?':['N/A']})
 
-df = df[['Name','Firm','Location','Email','Have Contacted?','Recieved Response?','Followed Up?']]
-
 df.head()
 
-networking  = networking.append(df, ignore_index= True)
+networking  = networking.append(df, ignore_index= True, sort = False)
 
-networking.sort_values(by = ['Name', 'Firm'], inplace=True)
+networking.head()
 
-networking = networking[['Name', 'Firm', 'Location', 'Email', 'Have Contacted?', 'Recieved Response?', 'Followed Up?']]
+networking.sort_values(by = ['Firm', 'Name'], inplace=True)
+
+networking.head()
 
 networking.duplicated().sum()
 
@@ -40,11 +40,11 @@ networking.to_csv('networking.csv', index = False)
 
 networking
 
-networking[networking['Firm'] == 'Duff & Phelps']
+networking[networking['Firm'] == 'Raymond James']
 
 networking['Location'][3:4] = 'Chicago, Illinois'
 networking['Email'][57:58] = 'rebecca.mcadams@cowen.com'
-networking['Have Contacted?'][77:78] = 'Yes'
-networking['Recieved Response?'][77:78] = 'No'
-networking['Followed Up?'][77:78] = 'No'
+networking['Have Contacted?'][81:82] = 'Yes'
+networking['Recieved Response?'][81:82] = 'No'
+networking['Followed Up?'][81:82] = 'No'
 networking[(networking['Have Contacted?'] == 'Yes') & (networking['Firm'] == 'Lazard')]
