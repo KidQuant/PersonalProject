f4a7cc4e64ef501aeec018423682c382bf2b5dec
Sealy <as70906n@pace.edu>
2018-09-27 21:07:36 -0400

    New code for new visual

--- Visualizations/Rolling_Beta/rolling_beta.py
+++ Visualizations/Rolling_Beta/rolling_beta.py
@@ -19,6 +19,22 @@ internet.plot('Date','Internet', label = 'Internet', color = 'red', xlim=('2010-
 software.plot('Date','Software', label = 'Software', color = 'orange', xlim =('2010-01-20','2018-09-13'), ax=ax)
 semiconductor.plot('Date', 'Semiconductor', label = 'Semiconductor', color='green', xlim =('2010-01-20','2018-09-13'), ax=ax)
 ax.set_title('Rolling Beta for Technology Sector', fontsize= 18, x = .2, y=1.02 )
-ax.set_xlabel('Source: Bloomberg LP', style = 'italic', x=.9, y=.9)
+ax.set_xlabel('Source: Bloomberg', style='italic', x=0.95)
+ax.legend(fontsize=10)
+plt.savefig('Visualizations/Rolling_Beta/rolling_beta.jpeg', bbox_inches='tight')
+
+financials = pd.read_csv('Visualizations/Rolling_Beta/financials_beta.csv')
+technology = pd.read_csv('Visualizations/Rolling_Beta/technology_beta.csv')
+
+financials['Date'] = pd.to_datetime(financials['Date'])
+technology['Date'] = pd.to_datetime(technology['Date'])
+
+technology.head()
+financials.tail()
+
+ax = financials.plot('Date', 'Financials', label = 'Financial', color = 'blue', xlim = ('2010-09-08', '2018-09-13'), figsize = (11,6))
+technology.plot('Date', 'Technology', label = 'Technology', color = 'red', xlim =('2010-09-08','2018-09-13'), ax=ax)
+ax.set_title('12-Month Beta: S&P 500 Tech, Financials', fontsize=18, x=.25, y=1.02)
+ax.set_xlabel('Source: Bloomberg', style = 'italic', x=0.95)
 ax.legend(fontsize=10)
 plt.savefig('Visualizations/Rolling_Beta/rolling_beta.jpeg', bbox_inches='tight')
