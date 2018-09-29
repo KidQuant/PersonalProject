261ac8f76f0cb81c0799b8357b257e8a72f5f076
Andre Sealy <andresealy@Andres-MacBook.local>
2018-09-29 11:51:58 -0400

    Changes to the width of the bars in the time series graphs

--- Visualizations/Gaming_Visuals/gaming_visuals.py
+++ Visualizations/Gaming_Visuals/gaming_visuals.py
@@ -6,7 +6,7 @@ gaming_trends = pd.DataFrame({'Average MAU': [65, 140, 190, 225, 260, 295,335,36
                               'Mobile Average MAU': [25, 70, 135, 185, 225, 248, 265, 330]},
                               index = ['2015A','2016A','2017A','2018E','2019E','2020E','2021E','2022E'])
 
-gaming_trends.plot(kind='bar', figsize = (11,5), ylim = (0, 500))
+gaming_trends.plot(kind='bar', figsize = (11,5), ylim = (0, 500), width = .8)
 plt.title('China Live Game Streaming MAU Forecast', x=.3, y=1.02, fontsize=20)
 plt.ylabel('In Millions', fontsize = 12)
 plt.xticks(rotation =0)
@@ -17,4 +17,9 @@ plt.savefig('Visualizations/Gaming_Visuals/gaming_trends.jpeg', bbox_inches='tig
 headset_trends = pd.DataFrame({'Gaming Peripheral': [1.5, 2.3, 2.9, 3.3, 4.2, 4.6, 5.9]},
                               index = ['2014A','2015A','2016A','2017A','2018E','2020E','2021E'])
 
-headset_trends.plot(kind = 'bar', figsize=(11,5))
+headset_trends.plot(kind = 'bar', figsize=(11,5), width= .8)
+plt.title('Online Gaming Peripheral Market', x = .25, y=1.02, fontsize=20)
+plt.xlabel('Source: Newzoo; Bloomberg', x=0.9, style='italic')
+plt.xticks(rotation=0, fontsize=11)
+plt.ylabel('In Billions', fontsize=12)
+plt.savefig('Visualizations/Gaming_Visuals/gaming_peripherals.jpeg', bbox_inches='tight')
