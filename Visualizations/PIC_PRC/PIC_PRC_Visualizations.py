f6c00d5f55b8ee92aaeea94e314c7317c6abfccc
Sealy <as70906n@pace.edu>
2018-11-25 19:03:30 -0500

    Updates to project.

--- Visualizations/PIC_PRC/PIC_PRC_Visualizations.py
+++ Visualizations/PIC_PRC/PIC_PRC_Visualizations.py
@@ -9,25 +9,26 @@ df = deficit[80:]
 df
 
 ax = df.plot('DATE', 'FYFSD', kind ='bar', color = 'blue', figsize = (12,6), label = 'Defict/Surplus' )
-ax.set_xlabel('Source: U.S. Treasury Department', style='italic', x=.89)
-ax.set_title('Surplus/Deficit After Tax Bill', fontsize=25, x=.2)
-
-
+ax.set_xlabel('Source: U.S. Treasury Department', style='italic', x=.79)
+ax.legend('')
+plt.savefig('Visualizations\PIC_PRC/deficit.png', bbox_inches='tight')
 
 df2 = pd.read_csv('Visualizations\PIC_PRC\GFDEGDQ188S.csv')
 
 df2['DATE'] = pd.to_datetime(df2['DATE'])
 
-ax1 = df2.plot('DATE', 'GFDEGDQ188S', label = 'Debt To GDP', color ='blue', xlim = ('1966-01-01', '18-06-01'), figsize = (12,6))
-ax1.set_title('Public Debt As A Percentage of GDP', fontsize=18, x=.2)
+ax1 = df2.plot('DATE', 'GFDEGDQ188S', label = 'Debt To GDP', color ='blue', xlim = ('1966-01-01', '2018-06-01'), figsize = (12,6))
+ax1.legend('')
+ax1.yaxis.set_major_formatter(mtick.PercentFormatter())
 ax1.axvspan('1970-01-01', '1971-01-01', color='grey', alpha=0.5)
 ax1.axvspan('1973-09-01', '1975-06-01', color='grey', alpha=0.5)
 ax1.axvspan('1980-01-01', '1980-09-01', color='grey', alpha=0.5)
 ax1.axvspan('1981-09-01', '1982-12-01', color='grey', alpha=0.5)
 ax1.axvspan('1990-09-01', '1991-03-01', color='grey', alpha=0.5)
-ax1.axvspan('01-04-01', '01-11-01', color='grey', alpha=0.5)
-ax1.axvspan('08-01-01', '09-07-01', color='grey', alpha=0.5)
+ax1.axvspan('2001-04-01', '2001-11-01', color='grey', alpha=0.5)
+ax1.axvspan('2008-01-01', '2009-07-01', color='grey', alpha=0.5)
 ax1.set_xlabel('Source: Federal Reserve Bank of St. Louis', style='italic', x=.75)
+plt.savefig('Visualizations/PIC_PRC/debttogdp.png', bbox_inches='tight')
 
 #Disposable Income
 
@@ -38,12 +39,13 @@ df3['DATE'] = pd.to_datetime(df3['DATE'])
 df3.head()
 df3.tail()
 
-ax2 = df3.plot('DATE', 'DSPIC96_PC1', label = 'Disposable Income', color = 'blue', xlim = ('00-01-01','18-09-01'), figsize = (12,6))
+ax2 = df3.plot('DATE', 'DSPIC96_PC1', label = 'Disposable Income', color = 'blue', xlim = ('2000-01-01','2018-09-01'), figsize = (12,6))
 ax2.yaxis.set_major_formatter(mtick.PercentFormatter())
-ax2.set_xlabel('Source: Bureau of Economic Analysis', style='italic', x=.75)
-ax2.axvspan('01-04-01', '01-11-01', color='grey', alpha=0.5)
-ax2.axvspan('08-01-01', '09-07-01', color='grey', alpha=0.5)
+ax2.set_xlabel('Source: Bureau of Economic Analysis', style='italic', x=.8)
+ax2.axvspan('2001-04-01', '2001-11-01', color='grey', alpha=0.5)
+ax2.axvspan('2008-01-01', '2009-07-01', color='grey', alpha=0.5)
 ax2.legend('')
+plt.savefig('Visualizations/PIC_PRC/disposableincome.png', bbox_inches = 'tight')
 
 #GDP Growth
 
@@ -54,16 +56,8 @@ df4 = df4[6:].reset_index()
 df4.drop(['index'], axis = 1, inplace = True)
 after = df4.copy()
 
-df4.head()
-after.head()
-
-df4.tail()
-after.tail()
-
 after['GDPC1_PCA'][0:14] = 0
 df4['GDPC1_PCA'][14:] = 0
-
-
 
 labels = ['Q2 14', 'Q3 14', 'Q4 14',
           'Q1 15', 'Q2 15', 'Q3 15', 'Q4 15',
@@ -77,3 +71,4 @@ ax3.yaxis.set_major_formatter(mtick.PercentFormatter())
 ax3.set_xticklabels(labels,rotation=0)
 ax3.legend('')
 ax3.set_xlabel('Source: Bureau of Economic Analysis', style='italic', x=.80)
+plt.savefig('Visualizations/PIC_PRC/GDP.png', bbox_inches ='tight')
