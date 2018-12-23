19510bfe15a592897014f2b8d069fd5b16537c86
Andre Sealy <andresealy@Andres-MacBook.local>
2018-12-23 16:51:37 -0500

    New additions

--- Practice/pyfund/words.py
+++ Practice/pyfund/words.py
@@ -1,15 +1,23 @@
 from urllib.request import urlopen
+import sys
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+        return story_words
 
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
+
+def main():
+    url = sys.argv[1]
+    words = fetch_words()
+    print_items(words)
 
 if __name__ == '__main__':
-    fetch_words()
+    main()
