a321ce3e4317c1cc8d968429c9e303c72bd50676
Andre <Andre@hedgology.com>
2018-12-29 09:07:50 -0500

    More small changes.

--- Practice/LearnPythonTheHardWay/ex21.py
+++ Practice/LearnPythonTheHardWay/ex21.py
@@ -28,6 +28,6 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 #A puzzle for the extra credit, type it in anyway.
 print("Here is a puzzle")
 
-what = add(age, subtract(height, multiply(weight, divide(iq,2))))
+what = add(age, substract(height, multiply(weight, divide(iq,2))))
 
 print("That becomes: ", what, "Can you do it by hand?")
