ec4950ff704ed98833d48b121da197b5891f837b
Andre <Andre@hedgology.com>
2019-06-12 00:49:28 -0400

    Small updates.

--- airbnb_competition/Model 1/zillow_airbnb_brooklyn.py
+++ airbnb_competition/Model 1/zillow_airbnb_brooklyn.py
@@ -1,4 +1,4 @@
-#%%
+#%% loading the packages
 
 import matplotlib
 import numpy as np
@@ -35,7 +35,6 @@ def reorder(location):
     return new
 
 nyc = pd.read_csv('listings.csv.gz')
-nyc.to_csv('listings.csv')
 nyc = read_data(nyc)
 nyc['price'] = nyc['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
 
@@ -323,8 +322,6 @@ y = df2['mean']
 n = (df2.index.tolist())
 
 fig, ax = plt.subplots()
-
-fig, ax = plt.subplots()
 ax.scatter(x, y, c=y,cmap = cmap, s =200)
 
 for i, txt in enumerate(n):
@@ -346,7 +343,7 @@ y = statistics['price']['mean'].convert_objects(convert_numeric=True)
 
 n = (x.reset_index().location.tolist())
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(11,9))
 ax.scatter(x, y, c=y,cmap = cmap , s =200)
 
 for i, txt in enumerate(n):
@@ -363,12 +360,12 @@ x = (365 - statistics['availability_365']['mean'].convert_objects(convert_numeri
 y = statistics['price']['mean'].convert_objects(convert_numeric = True).drop('Mill Basin')
 n = (x.reset_index()).location.tolist()
 
-fig, ax = plt.subplots(figsize=(9, 7))
+fig, ax = plt.subplots(figsize=(11, 9))
 ax.scatter(x, y, c=y,cmap = cmap , s =200)
 
 for i, txt in enumerate(n):
     ax.annotate(txt, (x[i],y[i]), fontsize = 15)
-    
+
 plt.xlabel('Average Number of Days booked in next 365 days', fontsize=18)
 plt.ylabel('Average Price', fontsize=18)
 plt.title('Average Price and Popularity of Listings', fontsize=18, fontweight='bold')
@@ -385,13 +382,13 @@ y = statistics['price']['mean'].convert_objects(convert_numeric=True)
 
 n = (x.reset_index()).location.tolist()
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(11,9))
 ax.scatter(x, y, c=y,cmap = cmap, s =200)
 
 for i, txt in enumerate(n):
     ax.annotate(txt, (x[i],y[i]), fontsize = 10)
-    
-    
+
+
 plt.xlabel('Average Rating', fontsize=14)
 plt.ylabel('Price', fontsize=14)
 plt.title('Price and Average Rating', fontsize=14, fontweight='bold')
@@ -463,8 +460,8 @@ ax.scatter(x, y, c=y,cmap = cmap, s =200)
 
 for i, txt in enumerate(n):
     ax.annotate(txt, (x[i],y[i]), fontsize = 16)
-    
-    
+
+
 plt.xlabel('Median Home Price', fontsize=20)
 plt.ylabel('Average Price ABNB Listing', fontsize=20)
 plt.title('Home Price(ZHVI) and Airbnb Listing Price', fontsize=20, fontweight='bold')
@@ -486,8 +483,8 @@ ax.scatter(x, y, c=y,cmap = cmap , s =200)
 
 for i, txt in enumerate(n):
     ax.annotate(txt, (x[i],y[i]), fontsize = 16)
-    
-plt.xlabel('5 Year Home Price Growth ', fontsize=20)    
+
+plt.xlabel('5 Year Home Price Growth ', fontsize=20)
 plt.ylabel('Average Price ABNB Listing', fontsize=20)
 plt.title('Home Price Growth, 5 Years and ABNB Listing Price', fontsize=20, fontweight='bold')
 
@@ -535,368 +532,4 @@ sns.heatmap(df.corr(), mask=mask, cmap=cmap, vmax=.3,
             square=True, xticklabels=2, yticklabels=5,
             linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
 
-#%% Regression Model for Predicting Average Price -- Using ZHVI and Airbnb Listing Data
-
-def read_data(location):
-       location = location[['zipcode', 'accommodates', 'bathrooms', 'bedrooms', 'price', 
-              'minimum_nights', 'availability_365', 'number_of_reviews', 'review_scores_rating',
-              'review_scores_accuracy', 'review_scores_value']].dropna(axis =0)
-       location = location.set_index('zipcode')
-       return location
-
-
-nyc = pd.read_csv('listings.csv.gz')
-bath_beach = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bath Beach'])
-bath_beach['price'] = bath_beach['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-nyc = pd.read_csv('listings.csv.gz')
-bath_beach = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bath Beach'])
-bath_beach['price'] = bath_beach['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-bay_ridge = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bay Ridge'])
-bay_ridge['price'] = bay_ridge['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-bed_stuy = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bedford-Stuyvesant'])
-bed_stuy['price'] = bed_stuy['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-bensonhurst = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bensonhurst'])
-bensonhurst['price'] = bensonhurst['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-bergen_beach = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bergen Beach'])
-bergen_beach['price'] = bergen_beach['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-boerum_hill = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Boerum Hill'])
-boerum_hill['price'] = boerum_hill['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-borough_park = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Borough Park'])
-borough_park['price'] = borough_park['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-brighton_beach = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Brighton Beach'])
-brighton_beach['price'] = brighton_beach['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-brooklyn_heights = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Brooklyn Heights'])
-brooklyn_heights['price'] = brooklyn_heights['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-brownsville = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Brownsville'])
-brownsville['price'] = brownsville['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-bushwick = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Bushwick'])
-bushwick['price'] = bushwick['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-canarsie = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Canarsie'])
-canarsie['price'] = canarsie['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-carroll_gardens = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Carroll Gardens'])
-carroll_gardens['price'] = carroll_gardens['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-clinton_hill = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Clinton Hill'])
-clinton_hill['price'] = clinton_hill['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-cobble_hill = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Cobble Hill'])
-cobble_hill['price'] = cobble_hill['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-columbia_st = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Columbia St'])
-columbia_st['price'] = columbia_st['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-coney_island = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Coney Island'])
-coney_island['price'] = coney_island['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-crown_heights = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Crown Heights'])
-crown_heights['price'] = crown_heights['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-cypress_hills = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Cypress Hills'])
-cypress_hills['price'] = cypress_hills['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-downtown_brooklyn = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Downtown Brooklyn'])
-downtown_brooklyn['price'] = downtown_brooklyn['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-dumbo = read_data(nyc[nyc['neighbourhood_cleansed'] == 'DUMBO'])
-dumbo['price'] = dumbo['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-dyker_heights = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Dyker Heights'])
-dyker_heights['price'] = dyker_heights['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-east_flatbush = read_data(nyc[nyc['neighbourhood_cleansed'] == 'East Flatbush'])
-east_flatbush['price'] = east_flatbush['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-east_newyork = read_data(nyc[nyc['neighbourhood_cleansed'] == 'East New York'])
-east_newyork['price'] = east_newyork['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-flatbush = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Flatbush'])
-flatbush['price'] = flatbush['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-flatlands = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Flatlands'])
-flatlands['price'] = flatlands['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-fort_greene = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Fort Greene'])
-fort_greene['price'] = fort_greene['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-fort_hamilton = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Fort Hamilton'])
-fort_hamilton['price'] = fort_hamilton['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-gowanus = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Gowanus'])
-gowanus['price'] = gowanus['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-gravesend = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Gravesend'])
-gravesend['price'] = gravesend['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-greenpoint = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Greenpoint'])
-greenpoint['price'] = greenpoint['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-kensington = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Kensington'])
-kensington['price'] = kensington['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-manhattan_beach = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Manhattan Beach'])
-manhattan_beach['price'] = manhattan_beach['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-midwood = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Midwood'])
-midwood['price'] = midwood['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-mill_basin = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Mill Basin'])
-mill_basin['price'] = mill_basin['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-navy_yard = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Navy Yard'])
-navy_yard['price'] = navy_yard['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-park_slope = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Park Slope'])
-park_slope['price'] = park_slope['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-prospect_heights = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Prospect Heights'])
-prospect_heights['price'] = prospect_heights['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-pros_leff = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Prospect-Lefferts Gardens'])
-pros_leff['price'] = pros_leff['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-red_hook = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Red Hook'])
-red_hook['price'] = red_hook['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-sea_gate = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Sea Gate'])
-sea_gate['price'] = sea_gate['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-sheepshead_bay = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Sheepshead Bay'])
-sheepshead_bay['price'] = sheepshead_bay['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-south_slope = read_data(nyc[nyc['neighbourhood_cleansed'] == 'South Slope'])
-south_slope['price'] = south_slope['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-sunset_park = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Sunset Park'])
-sunset_park['price'] = sunset_park['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-vinegar_hill = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Vinegar Hill'])
-vinegar_hill['price'] = vinegar_hill['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-williamsburg = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Williamsburg'])
-williamsburg['price'] = williamsburg['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-windsor_terrace = read_data(nyc[nyc['neighbourhood_cleansed'] == 'Windsor Terrace'])
-windsor_terrace['price'] = windsor_terrace['price'].map(lambda x: str(x)[1:]).convert_objects(convert_numeric=True)
-
-#DataFrame with all listings and Airbnb features:
-df_abnb = pd.concat([bath_beach, bay_ridge, bed_stuy, bensonhurst, bergen_beach, boerum_hill, borough_park, brighton_beach,
-              brooklyn_heights, brownsville, bushwick, canarsie, carroll_gardens, clinton_hill, cobble_hill, columbia_st,
-              coney_island, crown_heights, cypress_hills, downtown_brooklyn, dumbo, dyker_heights, east_flatbush, east_newyork,
-              flatbush, flatlands, fort_greene, fort_hamilton, gowanus, gravesend, greenpoint, kensington, manhattan_beach, 
-              midwood, mill_basin, park_slope, prospect_heights, pros_leff, red_hook, sunset_park, vinegar_hill, williamsburg,
-              windsor_terrace])
-
-df_abnb
-
-#DataFrame with Zillow Feature: Zillow Home Value Index
-ZHVI = pd.read_csv('Zip_Zhvi_Summary_AllHomes.csv').set_index('RegionName')
-ZHVI = ZHVI.ix[:,[7,11,12]]
-
-# Merge on Zipcode
-df1 = pd.merge(df_abnb, ZHVI, left_index = True, right_index = True).dropna()
-df1['reviewtotal'] = (df1['review_scores_rating'] + df1['review_scores_accuracy'] + df1['review_scores_value'])
-
-
-#%% Linearity of Features with Dependent Variable: Price / Availability and Pice / # of Reviews
-
-x = np.log(df1['availability_365'])
-y = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x,y, c ='c', alpha = .5)
-plt.ylim(0,1000)
-plt.xlim(-0.01, 1.75)
-
-plt.xlabel('Log of Availability', fontsize=14)
-plt.ylabel('Price', fontsize=14)
-plt.title('Price and Availability', fontsize=14, fontweight='bold')
-
-x1 = np.log(df1['number_of_reviews'])
-y1 = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x1, y1, c='r', alpha=.5)
-plt.ylim(0,1000)
-plt.xlim(-0.01, 1.75)
-
-plt.xlabel('Log of # of Reviews', fontsize=14)
-plt.ylabel('Price', fontsize=14)
-plt.title('Price and # of Reviews', fontsize=14, fontweight='bold')
-
-
-#%% #%% Linearity of Features with Dependent Variable: Price / Median Home Values
-
-x2 = (df1['Zhvi'])
-y2 = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x2, y2, c = 'r', alpha = 0.5)
-plt.ylim(0,1000)
-plt.xlim(200000,1400000)
-
-plt.xlabel('ZHVI (Median Home Price)', fontsize=14)
-plt.ylabel('Price', fontsize=14)
-plt.title('Price and Median Home Value (ZHVI)', fontsize=14, fontweight='bold')
-
-x = (df1['10Year'])
-y = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x, y, c ='c', alpha = 0.5)
-plt.ylim(0,1000)
-plt.xlim(-0.02, 0.06)
-
-plt.xlabel('10 Year Change in ZHVI (Median Home price)', fontsize=14)
-plt.ylabel('Price', fontsize=14)
-plt.title('Price and 10 Year Median Home Value (ZHVI)', fontsize=14, fontweight='bold')
-
-x1 = (df1['5Year'])
-y1 = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x1, y1, c= 'm', alpha = .5)
-
-plt.xlabel('5 Year Change in ZHVI (Median Home Price)', fontsize=14)   
-plt.ylabel('Price', fontsize=14)
-plt.title('Price and 5 Year Median Home Value (ZHVI)', fontsize=14, fontweight='bold')
-plt.ylim (0,1000)
-plt.xlim(.045,.15)
-
-x = (df1['reviewtotal'])
-y = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x, y, c= 'c', alpha = .5)
-plt.ylim (0,1000)
-plt.xlim(20 ,122)
-
-plt.xlabel('Sum of Ratings', fontsize=14)   
-plt.ylabel('Price', fontsize=14)
-plt.title('Sum of Ratings', fontsize=14, fontweight='bold')
-
-x1 = np.log(df1['bedrooms'])
-y1 = df1['price']
-
-fig, ax = plt.subplots()
-ax.scatter(x1, y1, c = 'r', alpha = .5)
-plt.ylim (0,1000)
-plt.xlim(-.1 ,2)
-
-plt.xlabel('# of Bedrooms', fontsize=14)   
-plt.ylabel('Price', fontsize=14)
-plt.title('# of Bedrooms and Price', fontsize=14, fontweight='bold')
-
-x1 = df1['bedrooms']
-x2 = df1['bathrooms']
-x3 = df1['accommodates']
-
-y1 = df1['price']
-
-
-fig, ax = plt.subplots()
-ax.scatter(x1, y1, c= 'c', alpha = .5)
-ax.scatter(x2, y1, c='r', alpha = .5)
-ax.scatter(x3, y1, c='m', alpha = .5)
-plt.ylim (-2,1000)
-plt.xlim(-.1 ,17)
-
-plt.xlabel('# of Bedrooms, Bathrooms, Accommodates', fontsize=14)   
-plt.ylabel('Price', fontsize=14)
-plt.title('# of Bedrooms/Bathroom/Accommodates and Price', fontsize=14, fontweight='bold')
-
-#%% The Model
-
-#The merged features dataframe
-df1 = pd.merge(df_abnb, ZHVI, left_index = True, right_index = True).dropna()
-df1['reviewtotal'] = (df1['review_scores_rating'] + df1['review_scores_accuracy'] + df1['review_scores_value'])
-
-#Trainsforming:
-df1['number_of_reviews'] = np.log(df1['number_of_reviews'])
-df1['availability_365'] = np.log(df1['availability_365'])
-
-#Final Features dataframe
-features = df1[['Zhvi', 'number_of_reviews', 'availability_365', 'reviewtotal', '10Year', '5Year',
-              'accommodates', 'bathrooms']].replace([np.inf, -np.inf], np.nan).fillna(0)
-
-features.corr()
-
-#%% Model Evaluation
-
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-
-X = features
-Y = pd.DataFrame(df1['price']).fillna(0)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-
-clf = LinearRegression()
-clf.fit(X_train, Y_train)
-
-print('R-squared on Training Data:', r2_score(Y_train, clf.predict(X_train)))
-print('R-squared on Testing Data:', r2_score(Y_test, clf.predict(X_test)))
-
-from sklearn.metrics import mean_squared_error
-
-print('R-squared on Test set:', mean_squared_error(Y_test, clf.predict(X_test)))
-print('R-squared on Testing Data:', mean_squared_error(Y_train, clf.predict(X_train)))
-
-coef_df = pd.concat([pd.DataFrame(features.columns), pd.DataFrame(clf.coef_).transpose()], axis=1)
-coef_df.columns = ['Feature', 'Regression Coefficient']
-coef_df
-
-import matplotlib.patches as mpatches
-
-plt.figure(figsize=(10,6))
-plt.scatter(clf.predict(X_train), clf.predict(X_train) - Y_train, c='c', s=40, alpha=0.5)
-plt.scatter(clf.predict(X_test), clf.predict(X_test) - Y_test, c='r', s=40, alpha=0.65)
-plt.hlines(y = 0, xmin=-30, xmax= 50)
-plt.title('Residuals vs Fitted Values')
-plt.ylabel('Residuals')
-plt.xlabel('Fitted Values')
-
-
-#%% Using the Random Forest Regression Model
-
-from sklearn.ensemble import RandomForestRegressor
-
-X = features
-Y = pd.DataFrame(df1['price']).fillna(0)
-
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
-
-rf = RandomForestRegressor(random_state=1)
-rf.fit(X_train, Y_train)
-
-print('R-squared for training data: ', rf.score(X_train, Y_train))
-print('R-squared for test data: ',rf.score(X_test, Y_test))
-
-from sklearn.metrics import mean_squared_error
-print('Mean squared error on the test set: ', mean_squared_error(Y_test, rf.predict(X_test)))
-print('Mean squared error on the training set: ', mean_squared_error(Y_train, rf.predict(X_train)))
-
-plt.scatter(rf.predict(X_test), Y_test.as_matrix())
-plt.title('Random Forest: Predicted vs. Actual Price')
-plt.xlabel('Predict Listing Price')
-plt.ylabel('Actual Listing Price')
-plt.xlim(0,1000)
-
-x = df1.price
-sns.distplot(x, kde=False)
-plt.title('Distribution of Price')
-
-
+#%%
