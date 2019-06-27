4d718e48ebaeb777762e49d390d347228092eb46
Andre Sealy <andre@hedgology.com>
2019-06-27 10:30:19 -0400

    new code for soccer hypothesis test

---
+++ DataScience/soccer_hypo_test/hypo_test.py
@@ -0,0 +1,9 @@
+import psycopg2
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import scipy
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.stats import ttest_ind
+from statsmodels.stats.power import TTestIndPower
