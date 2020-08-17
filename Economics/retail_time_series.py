ce279d013a5eab213f669ce7acede60616297fd6
KidQuant <andre@kidquant.com>
2020-08-17 00:34:17 -0400

    Updates

--- Economics/retail_time_series.py
+++ Economics/retail_time_series.py
@@ -7,24 +7,23 @@ alt.renderers.enable('altair_viewer')
 
 quandl.ApiConfig.api_key = 'tJKSQn1pHLbyo1mu1wYn'
 
-retailSales = quandl.get('FRED/RRSFS')
-retailSales.rename(columns={'Value': 'Retail Sales'}, inplace=True)
-retailSales['Change in Retail Sales'] = retailSales['Retail Sales'].diff()
+retailSales = quandl.get('FRED/RSNSR')
+retailSales.rename(columns={'Value': 'Online Sales'}, inplace=True)
+retailSales['Change in Online Sales'] = retailSales['Online Sales'].diff()
 retailSales.reset_index(inplace=True)
 
 base = alt.Chart(retailSales).encode(
     alt.X('Date:T',
-        axis=alt.Axis(title='Source: U.S. Census Bureau'),
-        textAlign='right')
+          axis=alt.Axis(title='Source: U.S. Census Bureau'))
 )
 
 
 bar = base.mark_bar().encode(
-    alt.Y('Change in Retail Sales',
+    alt.Y('Change in Online Sales',
           axis=alt.Axis(format='$,.0f',
                         title='Monthly Change (In millions)')),
     color=alt.condition(
-        alt.datum['Change in Retail Sales'] > 0,
+        alt.datum['Change in Online Sales'] > 0,
         alt.value("green"),
         alt.value("red")
     )
@@ -32,20 +31,20 @@ bar = base.mark_bar().encode(
 
 
 line = base.mark_line(color='black').encode(
-    alt.Y('Retail Sales',
+    alt.Y('Online Sales',
           axis=alt.Axis(format='$,.0f',
                         title='Retail Sales (In millions)'),
-          scale=alt.Scale(domain=[100000, 228000]))
+          scale=alt.Scale(domain=[0, 90000]))
 )
 
 alt.layer(bar, line).resolve_scale(
     y='independent'
 ).properties(
-    title='Monthly and Aggregate U.S. Retail Sales',
+    title='Monthly and Aggregate U.S. Online Sales',
     width=750,
-    height=400
+    height=450
 ).configure_title(
-    fontSize=16,
+    fontSize=18,
     anchor='start'
 ).configure_axis(
     labelFontSize=12
