76a01a7b6cb59e03546de33fcb56dd292c74e819
KidQuant <andre@kidquant.com>
2022-03-06 19:44:19 -0500

    Updated More Dynamic

--- Data Science/Warzone/test_extraction_wz.py
+++ Data Science/Warzone/test_extraction_wz.py
@@ -4,12 +4,13 @@ import pandas as pd
 import pickle
 from datetime import datetime
 import pandas as pd
+import os
 
 gamerTags = pd.read_csv('GamerTags.csv')
 gamerTags.head()
 
-user = gamerTags['User'][1]
-battleNet = gamerTags['BattleNet'][1]
+user = gamerTags['User'][2]
+battleNet = gamerTags['BattleNet'][2]
 
 url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/{}/battle/".format(battleNet)
 
@@ -51,11 +52,23 @@ df['Deaths'] = deaths
 df['KDRatio'] = kdRatio
 df.head()
 
+df = df[df['Mode'] != 'br_dmz_plunquad']
+df = df.reset_index().drop(columns= ['index'])
+
 import urllib.request
 
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
 
 headers={'User-Agent':user_agent,}
+
+
+# endpoint = "https://api.tracker.gg/api/v1/warzone/matches/10508390693456844744"
+# request = urllib.request.Request(endpoint, None, headers)
+# response = urllib.request.urlopen(request)
+# data = response.read()
+# jsonData = data.decode('utf8')
+# data = json.loads(jsonData)
+# avg = list(data['data']['attributes']['avgKd'].values())[0]
 
 avgKD = []
 matchDate = []
@@ -67,7 +80,7 @@ for i in df['MatchID']:
     response = urllib.request.urlopen(request)
 
     data = response.read()
-    jsonData = data.decode('utf8').replace("''", '"')
+    jsonData = data.decode('utf8')
 
     data = json.loads(jsonData)
 
@@ -84,23 +97,33 @@ for i in df['MatchID']:
     matchTime.append(time)
     avgKD.append(avg)
 
+
+
 df['LobbyKD'] = avgKD
 df['Date'] = matchDate
 df['Time'] = matchTime
 
-df.head()
+new = df
 
 with open('{}.pickle'.format(user), 'rb') as f:
+    print('Collecting old matches for: {}'.format(user))
     old = pickle.load(f)
-
 
 new = pd.concat([df, old], ignore_index=False)
 new.drop_duplicates(subset = 'MatchID', inplace=True)
+new = new.reset_index().drop(columns=['index'])
 
 with open('{}.pickle'.format(user), 'wb') as f:
+    print('Storing new matches for: {}'.format(user))
     pickle.dump(new, f)
 
 now = datetime.now()
 date_time =  now.strftime("%m-%d-%Y %H%H%S")
 
-df.to_csv("{}{}.csv".format(gamerTags['User'][1],date_time))
+savePath = 'Game History/' + str(user)
+
+if not os.path.exists('{}'.format(savePath)):
+    os.mkdir('{}'.format(savePath))
+
+
+df.to_csv("{}/{}{}.csv".format(savePath, user, date_time))
