f3dc3e23b5bcb18524a1758b5c3d6c51a2ea0484
Andre <andre@kidquant.com>
2022-06-06 15:07:51 -0400

    Update

--- Data Science/Warzone/lobby_kd.py
+++ Data Science/Warzone/lobby_kd.py
@@ -1,3 +1,4 @@
+
 import requests
 import urllib.parse
 import random
@@ -9,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 %matplotlib inline
+
 
 sso_token = "MTczNzE4NDQ4NDY2MzE4NDgxNTk6MTY1NTQ5ODUwNjExNjo4ODZmMmNjNTUwNzFlY2U4OTgwMTEzYzllOTMxZDhjMQ"
 cookies={"ACT_SSO_COOKIE": sso_token}
@@ -51,8 +53,31 @@ lobby_players_df
 
 lobby_players_df.info()
 
-playerurl = 'https://www.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/uno/uno/' + str(lobby_players[43]['player']['uno']) + '/profile/type/warzone'
 
-player = requests.request("GET", playerurl, cookies=cookies)
+headers = {
+	"X-RapidAPI-Host": "call-of-duty-modern-warfare.p.rapidapi.com",
+	"X-RapidAPI-Key": "e6c6bbe19amshdfe963bc74bf7dap1d5654jsn50ad5279af3c"
+}
 
-player.json()
+
+
+for i in range(len(lobby_players)):
+        time.sleep(4)
+        uno = lobby_players[i]['player']['uno']
+        user = lobby_players[i]['player']['username']
+        # print(user, uno)
+
+        url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/{}/uno".format(uno)
+        response = requests.request("GET", url, headers=headers)
+        print(response.json())
+
+
+        # if 'br' in response.json().keys():
+        #         kd = response.json()['br']['kdRatio'] 
+        #         print("{}: {}".format(user, kd))
+
+        # else:
+        #         print("NA Player")
+
+
+        # print(lobby_players[i]['player']['username'] + ': ' + lobby_players[i]['player']['uno'] )
