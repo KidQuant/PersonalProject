853caa7a23a6cdf1d9ec29caa5428fa4f1629218
KidQuant <andre@kidquant.com>
2022-06-15 09:27:47 -0400

    Updates

--- Data Science/Warzone/test_extraction_wz.py
+++ Data Science/Warzone/test_extraction_wz.py
@@ -16,7 +16,7 @@ PATH = 'C:\Program Files (x86)\chromedriver.exe'
 gamerTags = pd.read_csv('GamerTags.csv')
 gamerTags
 
-index = 9
+index = 58
 
 user = gamerTags['User'][index]
 gamerTag = gamerTags['GamerTag'][index]
