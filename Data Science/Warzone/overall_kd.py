2a3d5b229ebd8623c54be42ecb06183b178446e7
KidQuant <andre@kidquant.com>
2022-06-22 09:07:09 -0400

    Updates

--- Data Science/Warzone/overall_kd.py
+++ Data Science/Warzone/overall_kd.py
@@ -5,32 +5,44 @@ import os
 
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 
-user = 'Quants'
+user = 'Diaz Biffle'
 
 with open('User History\{}.pickle'.format(user), 'rb') as f:
-        print('Collecting old matches for: {}'.format(user))
-        df = pickle.load(f)
-        oldMatches = df['MatchID'].tolist()
-        print(df.head())
+    print('Collecting old matches for: {}'.format(user))
+    df = pickle.load(f)
 
-oldMatches
 
-endpoint = "https://api.tracker.gg/api/v1/warzone/matches/{}".format(oldMatches[0])
+# endpoint = 'https://api.tracker.gg/api/v1/warzone/matches/battlenet/diazbiffle%231415?/type=wz&next=null'
+# driver = webdriver.Chrome(PATH)
+# driver.get(endpoint)
 
-driver = webdriver.Chrome(PATH)
-driver.get(endpoint)
+# body = driver.find_element_by_xpath("/html/body").text
 
-body = driver.find_element_by_xpath("/html/body").text
+df = df[df['Date'] == '06-20-2022']
+oldMatches = df['MatchID'].tolist()
 
-data = json.loads(body)
-
-driver.quit()
-
-data['data']['attributes']['kdHistogram']
 
 hist = []
 
 for i in oldMatches:
-        
+
+        print(i)
+
+        endpoint = "https://api.tracker.gg/api/v1/warzone/matches/{}".format(i)
+        driver = webdriver.Chrome(PATH)
+        driver.get(endpoint)
+
+        body = driver.find_element_by_xpath("/html/body").text
+
+        data = json.loads(body)
+
+        driver.quit()
+
+        histogram = data['data']['attributes']['kdHistogram']
+
+        for obs in histogram:
+                print(obs)
+
+                hist.append(obs)
 
 
