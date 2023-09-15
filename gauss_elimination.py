78cb11eeec659e301e431a6e0c28faa78257340f
KidQuant <andre@kidquant.com>
2023-09-15 12:00:19 -0400

    Updates

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -1,13 +1,21 @@
 import numpy as np
 
 
-a_matrix = np.random.randint(-8, 0, size=(3,4))
-a_matrix
+a_matrix = np.random.randint(-8, 0, size=(3,5))
 
-np.delete(a_matrix, np.s_[-1:], axis=1)
 
-np.vsplit(a_matrix, 1)
-a_matrix[:][-1]
-b_matrix = [ele[3:] for ele in a_matrix]
-print(b_matrix)
-np.array([[-8], [-1], [-5]]) == b_matrix
+def matrix_check(matrix):
+    i = matrix.shape[0]
+    j = matrix.shape[1]
+
+    if i + 1 == j:
+        b_matrix = np.delete(a_matrix, np.s_[:-1], axis=1)
+        #  b_matrix = [ele[3:] for ele in a_matrix]
+        return a_matrix, b_matrix
+
+    else:
+        print('ERROR: Square matrix not given!')
+        return
+    
+
+matrix_check(a_matrix)
