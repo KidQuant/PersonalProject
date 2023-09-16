d5d3b6f8b56ddd60f195a1c29d0fdc929f6fce5d
KidQuant <andre@kidquant.com>
2023-09-16 19:02:29 -0400

    Updates

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -19,3 +19,19 @@ def matrix_check(matrix):
     
 
 matrix_check(a_matrix)
+
+n = len(np.delete(a_matrix, np.s_[:-1], axis=1))
+m = n - 1
+i = 0
+j = i - 1
+x = np.zeros(n)
+
+
+while i < n:
+    if a_matrix[i][i] == 0.0:
+        print("Divide by zero error!")
+        
+
+for j in range(i + 1, n):
+    print(i)
+
