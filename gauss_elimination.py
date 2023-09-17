3b22cd9e489393c5c4259fa501e6c812265af901
Stephanie <socraticallystephanie@gmail.com>
2023-09-17 01:03:08 -0400

    updates for homework.

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -1,37 +1,54 @@
 import numpy as np
 
 
-a_matrix = np.random.randint(-8, 0, size=(3,5))
+a_matrix = np.random.randint(-8, 8, size=(4,5))
+a_matrix
 
+def guass_elimination(matrix):
 
-def matrix_check(matrix):
-    i = matrix.shape[0]
-    j = matrix.shape[1]
+    rows = matrix.shape[0]
+    columns = matrix.shape[1]
 
-    if i + 1 == j:
-        b_matrix = np.delete(a_matrix, np.s_[:-1], axis=1)
-        #  b_matrix = [ele[3:] for ele in a_matrix]
-        return a_matrix, b_matrix
-
-    else:
+    if rows + 1 != columns:
         print('ERROR: Square matrix not given!')
         return
-    
 
-matrix_check(a_matrix)
+    else:
 
-n = len(np.delete(a_matrix, np.s_[:-1], axis=1))
-m = n - 1
-i = 0
-j = i - 1
-x = np.zeros(n)
+        n = len(np.delete(matrix, np.s_[:-1], axis=1))
+        m = n - 1
+        i = 0
+        j = i - 1
+        x = np.zeros(n)
+
+        while i < n:
+
+            if matrix[i][i] == 0.0:
+                print("Divide by zero error!")
+
+            for j in range(i + 1, n):
+                # print(i, j)
+                scalingFactor = matrix[j][i] / matrix[i][i]
+                matrix[j] = matrix[j] - (scalingFactor * matrix[i])
+                # print(matrix)
+                # print('')
+
+            i += 1
+
+        x[m] = matrix[m][n] / matrix[m][m]
+
+        for k in range(n-2, -1, -1):
+            x[k] = matrix[k][n]
+
+            for j in range(k+1, n):
+                x[k] = x[k] - matrix[k][j] * x[j]
+                print(k, x[k])
+
+            x[k] = x[k] / matrix[k][k]
+
+        for an in range(n):
+
+            print(f'x{an + 1} = {x[an]}')
 
 
-while i < n:
-    if a_matrix[i][i] == 0.0:
-        print("Divide by zero error!")
-        
-
-for j in range(i + 1, n):
-    print(i)
-
+guass_elimination(a_matrix)
