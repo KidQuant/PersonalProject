abf74efe32ec4e37cd7ea2c473d4578dc17df947
KidQuant <38800128+KidQuant@users.noreply.github.com>
2024-03-23 01:48:36 -0400

    updates

---
+++ Algorithms/code_fragment.py
@@ -0,0 +1,9 @@
+class CreditCard:
+    """A consumer credit card."""
+
+    def __init__(self, customer, bank, acnt, limit):
+        """Create a new credit card instance.
+
+        The initial balance is zero.
+
+        """
